8c5f6bafc5bf08aca6c3d0cf5a54586a8a788dd3
Hashir Dilmurad Zai <dzhashirdilmuradzai12@gmail.com>
2026-09-23 13:23:38 +0500

    JavaScript Fetch API

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 apiKey = "66abf78bb83140e29a1150945261509";
 const weather = document.querySelector("#weather");
 const humidity = document.querySelector("#humidity");
+const loading = document.querySelector("#loading");
+const errorMessage = document.querySelector("#error");
+const weatherIcon = document.querySelector("#weather-icon");
+const weatherCondition = document.querySelector("#weather-condition");
 
 async function weatherData(event) {
   try {
@@ -8,17 +12,43 @@ async function weatherData(event) {
 
     const input = document.querySelector("input").value;
 
+    loading.innerHTML = `Loading...`;
+
+    if (!input) {
+      loading.innerHTML = "";
+      weather.innerHTML = "";
+      humidity.innerHTML = "";
+      weatherIcon.src = "";
+      weatherCondition.innerHTML = "";
+      errorMessage.innerHTML = `Please enter city name`;
+      return;
+    }
+
     const apiData = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${input}`,
     );
 
-    const data = await apiData.json();
+    const apiData2 = await apiData.json();
 
-    weather.innerHTML = `Temperature is: ${data.current.temp_c}`;
-    humidity.innerHTML = `Humidity is: ${data.current.humidity}`;
+    if (apiData2.error) {
+      throw new Error(apiData2.error.message);
+    }
 
-    console.log(data.response);
+    console.log(apiData2);
+
+    loading.innerHTML = "";
+    errorMessage.innerHTML = "";
+    weather.innerHTML = `Temperature is: ${apiData2.current.temp_c}`;
+    humidity.innerHTML = `Humidity is: ${apiData2.current.humidity}`;
+    weatherIcon.src = `${apiData2.current.condition.icon}`;
+    weatherCondition.innerHTML = `${apiData2.current.condition.text}`;
   } catch (error) {
-    console.log(error.response);
+    loading.innerHTML = "";
+    weather.innerHTML = "";
+    humidity.innerHTML = "";
+    weatherIcon.src = "";
+    weatherCondition.innerHTML = "";
+
+    errorMessage.innerHTML = `${error.message}`;
   }
 }
